09edb824db342ab50c7b28c46a414ce8a8b0239a
Connor Steel <csteel217@gmail.com>
2025-11-18 23:28:30 +1300

    finished chap 5

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -12,6 +12,10 @@
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
 
+fn calculate_price_of_apples(a: u32) -> u32 {
+    if a > 40 { a } else { a * 2 }
+}
+
 fn main() {
     // You can optionally experiment here.
 }
